76cdd6a7dc3a767d693fd99399ee5c4d6473ce2c
strix25 <damjan.oslaj@gmail.com>
2018-01-18 19:36:38 +0100

    .
    .

---
+++ compare-react/src/components/Header/Header.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: false};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOne: !prevState.isToggleOn
+        }));
+    }
+  
+    render() {
+    
+        let menuActive = this.state.isToggleOn ? 'is-active' : '';
+
+    return (
+        <div className="navbar has-shadow">
+        <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item">MyCompany</a>
+        </div>
+
+        <button className={"button navbar-burger " +menuActive} onClick={this.handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div className={'navbar-end navbar-menu '+menuActive}>
+
+          <Link to="/" className="navbar-item r-item">Home</Link>
+          <Link to="/faq" className="navbar-item r-item">Features</Link>
+          <Link to="/faq" className="navbar-item r-item">About</Link>
+          <Link to="/faq" className="navbar-item r-item">FAQ</Link>
+          
+          <div className="navbar-item">
+            <p className="control">
+              <a className="button is-primary is-outlined">
+                <span className="icon">
+                  <i className="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Header;
---
+++ compare-react/src/components/Home/Home.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import './Home.css';
+
+export default class Home extends Component {
+    render() {
+        let heading = "Soaring to new heights";
+        let subheading = "Lorem  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum";
+        return (
+            <div>
+                <section className="hero">
+                    <div className="hero-body">
+                        <div className="container">
+                        <h1 Name="title">{ heading }</h1>
+                        <div className="is-two-thirfs column is-paddingless">
+                            <h2 className="subtitle is-4"> { subheading }</h2>
+                        </div>
+                        <a className="button is-large is-primary" id="learn">Learn more</a>
+
+                        </div>
+                    </div>
+                </section>
+
+                <section className="section">
+                    <div className="container">
+                        <div className="columns pd is-desktop">
+                        <div className="column is-1 has-text-centered">
+                            <i className="fa fa-cog is-primary"></i>
+                        </div>
+                        <div className="column is-one-third-desktop has-text-centered">
+                            <p className="title"><strong>We provide superior logistics so that your business can secceed in a crazy world</strong></p>
+                        </div>
+                        <div className="column">
+                            <p>We provide superior logistics so that your business can secceed in a crazy world</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div className="columns pd">
+                        <div className="column">
+                        <div className="card">
+                            <div className="card-content">
+                            <p className="title">"I think it's an absolutely excellent tool for out business. I can't survive without this ting."</p>
+                            <p className="subtitle">Damjan Oslaj</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="column">
+                        <div className="card">
+                            <div className="card-content">
+                            <p className="title">"I think it's an absolutely excellent tool for out business. I can't survive without this ting."</p>
+                            <p className="subtitle">Damjan Oslaj</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="column">
+                        <div className="card">
+                            <div className="card-content">
+                            <p className="title">"I think it's an absolutely excellent tool for out business. I can't survive without this ting."</p>
+                            <p className="subtitle">Damjan Oslaj</p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                 </section>
+
+            </div>
+        )
+    }
+}
